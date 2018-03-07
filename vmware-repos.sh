d215da5b7f1b9231b1500a18087b38bf5040fc85
Andrew Chin <andrewchin@vmware.com>
2018-03-07 15:03:41 -0600

    Update clone script

--- vmware-repos.sh
+++ vmware-repos.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
+if [ -z "$GITHUB_USER" ]; then
+  echo "Must export GITHUB_USER to clone repos"
+  echo "export GITHUB_USER=vmware to clone upstream repo"
+  exit 1
+fi
+
+function test_repo() {
+  STATUS=$(curl -s -o /dev/null -I -w "%{http_code}" https://github.com/$GITHUB_USER/$1)
+  if [ "$STATUS" == "200" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 echo "Cloning VIC"
-echo "[ENTER] to clone vmware/vic instead of your fork"
-read -p 'Enter your Github username: ' USERNAME
-USERNAME=${USERNAME:-vmware}
-git clone git@github.com:${USERNAME}/vic.git ~/go/src/github.com/vmware/vic
+if [ test_repo vic ]; then
+  git clone git@github.com:${GITHUB_USER}/vic.git ~/go/src/github.com/vmware/vic
+else
+  echo "Failed to find repo"
+  exit 1
+fi
 
 cd ~/go/src/github.com/vmware/vic
 git remote add upstream git@github.com:vmware/vic.git
 curl https://cdn.rawgit.com/tommarshall/git-good-commit/v0.6.1/hook.sh > .git/hooks/commit-msg && chmod +x .git/hooks/commit-msg
 
 echo "Cloning vic-product"
-echo "[ENTER] to clone vmware/vic-product instead of your fork"
-read -p 'Do you have a vic-product fork? [y/N] ' VIC_PRODUCT
-VIC_PRODUCT=${VIC_PRODUCT:-N}
-
-if [ "$VIC_PRODUCT" == "N" ]; then
-  USERNAME="vmware"
+if [ test_repo vic-product ]; then
+  git clone git@github.com:${GITHUB_USER}/vic-product.git ~/go/src/github.com/vmware/vic-product
+else
+  echo "Failed to find repo, cloning upstream"
+  git clone git@github.com:vmware/vic-product.git ~/go/src/github.com/vmware/vic-product
 fi
-
-git clone git@github.com:${USERNAME}/vic-product.git ~/go/src/github.com/vmware/vic-product
 
 cd ~/go/src/github.com/vmware/vic-product
 git remote add upstream git@github.com:vmware/vic-product.git
